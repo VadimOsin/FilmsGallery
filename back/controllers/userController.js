eb3f6f99f2f3c289e5501b76f6e607b4a173f86f
VadimOsin <vadzim.osin2001@gmail.com>
2023-03-23 10:20:01 +0300

    add authMiddleware + user function login, registration + check auth

--- back/controllers/userController.js
+++ back/controllers/userController.js
@@ -13,41 +13,51 @@ const generateJwt = (id, email, role) => {
 
 class UserController {
     async registration(req, res, next) {
-        // const {email, password, role} = req.body
-        // if (!email || !password) {
-        //     return next(ApiError.badRequest('Некорректный email или password'))
-        // }
-        // const candidate = await User.findOne({where: {email}})
-        // if (candidate) {
-        //     return next(ApiError.badRequest('Пользователь с таким email уже существует'))
-        // }
-        // const hashPassword = await bcrypt.hash(password, 5)
-        // const user = await User.create({email, role, password: hashPassword})
-        // const basket = await Basket.create({userId: user.id})
-        // const token = generateJwt(user.id, user.email, user.role)
-        // return res.json({token})
-        return res.json({message:'ok'})
+        try {
+            const {email, password, role} = req.body
+            if (!email || !password) {
+                return next(ApiError.badRequest('Некорректный email или password'))
+            }
+            const candidate = await db.query(`SELECT *
+                                              FROM "user"
+                                              where email = $1`, [email])
+            if (candidate.rows[0]) {
+                return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+            }
+            const hashPassword = await bcrypt.hash(password, 5)
+            const user = await db.query(`INSERT INTO "user" (email, password, role)
+                                         values ($1, $2, $3) RETURNING *`, [email, hashPassword, role])
 
+            const token = generateJwt(user.rows[0].id, user.rows[0].email, user.rows[0].role)
+            return res.json({token})
+        } catch (e) {
+            res.status(400).json({message: "registration error"})
+        }
     }
 
     async login(req, res, next) {
-        // const {email, password} = req.body
-        // const user = await User.findOne({where: {email}})
-        // if (!user) {
-        //     return next(ApiError.internal('Пользователь не найден'))
-        // }
-        // let comparePassword = bcrypt.compareSync(password, user.password)
-        // if (!comparePassword) {
-        //     return next(ApiError.internal('Указан неверный пароль'))
-        // }
-        // const token = generateJwt(user.id, user.email, user.role)
-        // return res.json({token})
-        return res.json({message:'ok'})
+        try {
+            const {email, password} = req.body
+            const user = await db.query(`SELECT *
+                                         FROM "user"
+                                         where email = $1`, [email])
+            if (!user.rows[0]) {
+                return next(ApiError.internal('Пользователь не найден'))
+            }
+            let comparePassword = bcrypt.compareSync(password, user.rows[0].password)
+            if (!comparePassword) {
+                return next(ApiError.internal('Указан неверный пароль'))
+            }
+            const token = generateJwt(user.rows[0].id, user.rows[0].email, user.rows[0].role)
+            return res.json({token})
+        } catch (e) {
+            res.status(400).json({message: "login error"})
+        }
     }
 
     async check(req, res, next) {
-        //
-        return res.json({message:'ok'})
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
     }
 }
 
