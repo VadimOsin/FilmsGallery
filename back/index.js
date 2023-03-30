1edb7c76f046c6444f68dc3e6c18543c23255dfd
VadimOsin <vadzim.osin2001@gmail.com>
2023-03-30 16:24:04 +0300

    add static

--- back/index.js
+++ back/index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.resolve(__dirname, `static/${'user'||'film'}`)))
 app.use(fileUpload({}))
 app.use('/api', router)
 app.use(errorHandler)
