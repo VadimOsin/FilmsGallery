c6ef23a756af5ae3d7b3852e4f3b7890d488d37b
VadimOsin <vadzim.osin2001@gmail.com>
2023-04-07 12:12:39 +0300

    add loading, resetting the page when receiving an update

--- front/film_gallery/src/modules/ListFilm/ListFilm.tsx
+++ front/film_gallery/src/modules/ListFilm/ListFilm.tsx
@@ -6,21 +6,36 @@ import {IFilm} from "../../global/types/types";
 import Pagination from "../../components/Pagination/Pagination";
 import YearSelect from "../../components/YearSelect/YearSelect";
 import RatingSelect from "../../components/RatingSelect/RatingSelect";
+import SearchWorlds from "../../components/SecrchWorlds/SearchWorlds";
+import Spinner from "../../components/Spinner/Spinner";
 
 const ListFilm = () => {
         let date = new Date().getFullYear()
         const [films, setFilms] = useState<IFilm[]>([])
         const [pages, setPages] = useState(1)
-        const [year, setYear] = useState(`&yearFrom=1980&yearTo=${date}`)
-        const [rating, setRating] = useState('')
+        const [year, setYear] = useState(`&yearFrom=1500&yearTo=${date}`)
+        const [rating, setRating] = useState(10)
+        const [str, setStr] = useState('')
+        const [totalPages, setTotalPages] = useState(5)
+        const [loading, setLoading] = useState(true)
         useEffect(() => {
-            getFilms().then((response) => {
-                setFilms(response)
-            })
-        }, [])
-        useEffect(() => {
+            setLoading(true)
+            setTimeout(() => {
+                getFilms({
+                    page: pages,
+                    limit: 12,
+                    category: "ALL",
+                    ratingTo: rating,
+                    year: year,
+                    keyword: str
+                }).then((response) => {
+                    setFilms(response.films)
+                    setTotalPages(response.totalPages)
+                }).finally(() => setLoading(false))
+            }, 1000)
+        }, [year, rating, pages, str])
 
-        }, [year, rating])
+
         // useEffect  (() => {
         // const film: IFilm = {
         //     id: fil?.kinopoiskId ?? Math.floor(Math.random() * 1000000),
@@ -59,30 +74,37 @@ const ListFilm = () => {
         }
         return (<>
                 <div className="filmList__sort">
-                    <YearSelect date={date} setYear={setYear}/>
-                    <RatingSelect setRating={setRating}/>
+                    <SearchWorlds str={str} setStr={setStr} setPages={setPages}/>
+                    <YearSelect date={date} setYear={setYear} setPages={setPages}/>
+                    <RatingSelect setRating={setRating} setPages={setPages}/>
                 </div>
-                <div className='filmList'>
-
-                    {Array.isArray(films) && films.map((film) =>
-                        <FilmItem
-                            key={film.id}
-                            id={film.id}
-                            nameoriginal={film.nameoriginal}
-                            posterurlpreview={film.posterurlpreview}
-                            ratingkinopoisk={film.ratingkinopoisk}
-                            year={film.year}
-                            genres={film.genres}
-                            countries={film.countries}
-                            type={film.type}
-                            filmlength={film.filmlength}
-                            ratingagelimits={film.ratingagelimits}
-                            descriptions={film.descriptions}
-                            nameen={film.nameen}
-                            nameru={film.nameru}/>
-                    )}
-                </div>
-                <Pagination active={pages} setPage={setPages} totalPages={5}/>
+                {loading ? (
+                        <Spinner/>
+                ) : (
+                    <>
+                        <div className='filmList'>
+                            {films?.map((film) => (
+                                <FilmItem
+                                    key={film.id}
+                                    id={film.id}
+                                    nameoriginal={film.nameoriginal}
+                                    posterurlpreview={film.posterurlpreview}
+                                    ratingkinopoisk={film.ratingkinopoisk}
+                                    year={film.year}
+                                    genres={film.genres}
+                                    countries={film.countries}
+                                    type={film.type}
+                                    filmlength={film.filmlength}
+                                    ratingagelimits={film.ratingagelimits}
+                                    descriptions={film.descriptions}
+                                    nameen={film.nameen}
+                                    nameru={film.nameru}
+                                />
+                            ))}
+                        </div>
+                        <Pagination active={pages} setPage={setPages} totalPages={totalPages} />
+                    </>
+                )}
             </>
         );
     }
