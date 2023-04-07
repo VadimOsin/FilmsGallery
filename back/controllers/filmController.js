c6eccc80443c5c1b13ec2935c2627a9c3ac8b7fa
VadimOsin <vadzim.osin2001@gmail.com>
2023-04-07 10:40:36 +0300

    add query params(yearTo,yearFrom,RatingTo,keyword)

--- back/controllers/filmController.js
+++ back/controllers/filmController.js
@@ -35,14 +35,125 @@ class filmController {
     }
 
     async getAllFilms(req, res) {
-
         try {
-            const film = await db.query(`SELECT *
-                                         FROM film`)
-            res.json(film.rows)
-        } catch (err) {
-            console.error(err)
-            res.status(500).json({message: 'Failed getAll film' + err})
+            let {
+                category = "ALL",
+                ratingTo = 10,
+                keyword = "",
+                yearFrom = 1980,
+                yearTo = 2023,
+                page = 1,
+                limit = 10,
+            } = req.query;
+
+            // calculate offset based on page and limit
+            const offset = (page - 1) * limit;
+
+            // build SQL query
+            let query = `SELECT *
+                         FROM film
+                         WHERE year BETWEEN ${yearFrom}
+                           AND ${yearTo}`;
+
+            if (category !== "ALL") {
+                query += ` AND type='${category}'`;
+            }
+
+            if (ratingTo < 10) {
+                query += ` AND ratingkinopoisk <= ${ratingTo}`;
+            }
+
+            if (keyword !== "") {
+                query += ` AND (nameru ILIKE '%' || $1 || '%' OR nameen ILIKE '%' || $1 || '%' OR nameoriginal ILIKE '%' || $1 || '%')`;
+            } else {
+                query += ` AND ($1::text IS NULL OR (nameru ILIKE '%' || $1 || '%' OR nameen ILIKE '%' || $1 || '%' OR nameoriginal ILIKE '%' || $1 || '%'))`;
+            }
+
+            query += ` ORDER BY id LIMIT ${limit} OFFSET ${offset}`;
+
+            // execute query and count total number of rows
+            const {rows} = await db.query(query, [keyword]);
+
+            let countQuery;
+
+            if (category !== "ALL" && ratingTo < 10 && keyword !== "") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND type ='${category}'
+                                               AND ratingkinopoisk <= ${ratingTo}
+                                               AND (nameru ILIKE '%' || $1 || '%'
+                                                OR nameen ILIKE '%' || $1 || '%'
+                                                OR nameoriginal ILIKE '%' || $1 || '%')`, [keyword]);
+            } else if (category !== "ALL" && ratingTo < 10) {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND type ='${category}'
+                                               AND ratingkinopoisk <= ${ratingTo}`);
+            } else if (category !== "ALL" && keyword !== "") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND type ='${category}'
+                                               AND (nameru ILIKE '%' || $1 || '%'
+                                                OR nameen ILIKE '%' || $1 || '%'
+                                                OR nameoriginal ILIKE '%' || $1 || '%')`, [keyword]);
+            } else if (ratingTo < 10 && keyword !== "") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND type ='${category}'
+                                               AND (nameru ILIKE '%' || $1 || '%'
+                                                OR nameen ILIKE '%' || $1 || '%'
+                                                OR nameoriginal ILIKE '%' || $1 || '%')`, [keyword]);
+            } else if (ratingTo < 10 && keyword !== "") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND ratingkinopoisk <= ${ratingTo}
+                                               AND (nameru ILIKE '%' || $1 || '%'
+                                                OR nameen ILIKE '%' || $1 || '%'
+                                                OR nameoriginal ILIKE '%' || $1 || '%')`, [keyword]);
+            } else if (category !== "ALL") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND type ='${category}'`);
+            } else if (ratingTo < 10) {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND ratingkinopoisk <= ${ratingTo}`);
+            } else if (keyword !== "") {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}
+                                               AND (nameru ILIKE '%' || $1 || '%'
+                                                OR nameen ILIKE '%' || $1 || '%'
+                                                OR nameoriginal ILIKE '%' || $1 || '%')`, [keyword]);
+            } else {
+                countQuery = await db.query(`SELECT COUNT(*)
+                                             FROM film
+                                             WHERE year BETWEEN ${yearFrom}
+                                               AND ${yearTo}`);
+            }
+
+            const totalRows = parseInt(countQuery.rows[0].count);
+            const totalPages = Math.ceil(totalRows / limit);
+
+            res.status(200).json({films: rows, totalPages: totalPages});
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({message: "Server Error"});
         }
     }
 
